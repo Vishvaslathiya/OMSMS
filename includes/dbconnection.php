ecd73956b2dee04f12a399a4fbd4ce71db555b04
vishvaslathiya <lathiyavishvas@gmail.com>
2023-11-11 21:46:47 +0530

    first commit

---
+++ includes/dbconnection.php
@@ -0,0 +1,3 @@
+<?php
+echo"COnnection to database";
+?>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+
+<footer class="footer"style="margin:auto; background: transperent;">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
+            <?php echo date('Y'); ?> All rights reserved.
+        </span>
+
+    </div>
+</footer>
